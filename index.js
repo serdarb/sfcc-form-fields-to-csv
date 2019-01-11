4d6b4031d847c210315acb09d731a772e0cc2e6d
Mustafa İlhan <ilhan.mstf@gmail.com>
2019-01-11 17:07:22 +0300

    dotenv fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const { load } = require('dotenv');
+const dotenv = require('dotenv');
 
 const ocapi = require('./ocapi-calls');
 const webdav = require('./webdav-calls');
 const formFileUtil = require('./form-file-util');
 
-load();
+dotenv.load();
 
 (async ({ BASE_URL, BM_USER, BM_PASS, CLIENT_ID, CLIENT_KEY, SITE_ID }) => {
     try {
@@ -14,8 +14,11 @@ load();
         const activeCodeVersion = await ocapi.getActiveCodeVersion(BASE_URL, token);
         const cartridges = await ocapi.getSiteCartridges(BASE_URL, SITE_ID, token);
         const locales = await ocapi.getSiteLocales(BASE_URL, SITE_ID, token);
-
+        
+        console.log('\r\nretrived site information from ocapi\r\n');
         await webdav.getFormXMLsOfSiteCartridges(BASE_URL, BM_USER, BM_PASS, activeCodeVersion, cartridges, locales);
+        
+        console.log('downloaded form files from webdav\r\n');
         await formFileUtil.processFiles();      
 
         console.log('\r\ncompleted!\r\n');
